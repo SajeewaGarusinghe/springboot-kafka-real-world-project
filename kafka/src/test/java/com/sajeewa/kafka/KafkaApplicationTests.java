1b4d23607450a9da5f083e664d0998bb6bc6e797
sajeewa garusinghe <sajeeva616@gmail.com>
2023-07-26 07:38:51 +0530

    initial commit

---
+++ kafka/src/test/java/com/sajeewa/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sajeewa.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
